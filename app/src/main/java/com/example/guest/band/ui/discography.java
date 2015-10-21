7389104a087a7034015d03855f7beb7806d2491d
ccmxy <windrainicefire@gmail.com>
2015-10-21 14:24:23 -0700

    Album images also showing

--- app/src/main/java/com/example/guest/band/ui/discography.java
+++ app/src/main/java/com/example/guest/band/ui/discography.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.guest.band.R;
@@ -14,6 +15,7 @@ import com.example.guest.band.models.AlbumLib;
 
 public class discography extends AppCompatActivity {
     private TextView mAlbumName;
+    private ImageView mAlbumImage;
     private Button mNextButton;
     private Album mCurrentAlbum;
     private AlbumLib mAlbumLib;
@@ -22,21 +24,27 @@ public class discography extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_discography);
+       setContentView(R.layout.activity_discography);
 
         mAlbumName = (TextView) findViewById(R.id.albumNameText);
+        mAlbumImage = (ImageView) findViewById(R.id.albumImage);
         mNextButton = (Button) findViewById(R.id.nextDiscButton);
         mAlbumLib = new AlbumLib();
         mCurrentAlbum = mAlbumLib.getAlbums().get(0);
-        mAlbumName.setText(mCurrentAlbum.getName());
+        setTheContent();
 
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mCurrentAlbum = mAlbumLib.nextAlbum(mCurrentAlbum);
-                mAlbumName.setText(mCurrentAlbum.getName());
+                setTheContent();
             }
         });
+    }
+
+    private void setTheContent() {
+        mAlbumName.setText(mCurrentAlbum.getName());
+        mAlbumImage.setImageResource(mCurrentAlbum.getImage());
     }
 
 }
